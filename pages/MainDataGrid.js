b1b96a011adabfc35e1a7ec74642f5498591aefe
freddie <freddie@unknownf4d4887bc1bf.attlocal.net>
2022-10-22 19:55:21 -0700

    initial

--- pages/MainDataGrid.js
+++ pages/MainDataGrid.js
@@ -28,6 +28,7 @@ export default function MainDataGrid(props) {
 
         <div style={{ height: "80vh", width: '90%', marginLeft: "auto", marginRight: "auto", marginTop: "2.5vh" }}>
             <DataGridPremium rows={rows} columns={columns}  components={{ Toolbar: GridToolbar }}/>
+            <h1>ds</h1>
         </div>
     );
 }
